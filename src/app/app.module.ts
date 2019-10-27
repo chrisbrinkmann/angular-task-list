3fe3913d5759119fd8c65fbd6dcab815c2abd18d
chrisbrinkmann <brinkmcd@gmail.com>
2019-10-27 15:15:30 -0400

    Correct typo in directory stucture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './components/tasks/tasks.component';
-import { TaskItemComponent } from './componenets/task-item/task-item.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
