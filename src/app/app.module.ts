124ac97d227db5bb1bdf0d8611379fd573f3cdc8
chrisbrinkmann <brinkmcd@gmail.com>
2019-10-27 16:45:52 -0400

    Implement add task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { TasksComponent } from './components/tasks/tasks.component'
-import { TaskItemComponent } from './components/task-item/task-item.component';
+import { TaskItemComponent } from './components/task-item/task-item.component'
 import { HeaderComponent } from './components/layout/header/header.component'
+import { AddTaskComponent } from './components/add-task/add-task.component'
 
 @NgModule({
-  declarations: [AppComponent, TasksComponent, TaskItemComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    TasksComponent,
+    TaskItemComponent,
+    HeaderComponent,
+    AddTaskComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
